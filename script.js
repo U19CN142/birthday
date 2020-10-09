4fba48ef74e61ca49a54bda6ced1ea7e789a8fef
U19CN142 <66122695+U19CN142@users.noreply.github.com>
2020-10-09 20:58:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Varun Madarapu"&&b=="15102001")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
